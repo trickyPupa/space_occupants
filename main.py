17898a7876c7ff5c28fd33b8e48cfa1c5ce487a4
timof <timofeylabor@gmail.com>
2021-02-01 21:04:35 +0300

    5

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 from ships import *
-from random import choice
 
 
 def main():
-    screen = pygame.display.set_mode(SIZE)
+    screen = pygame.display.set_mode((0, 0))
+    SIZE = pygame.display.get_window_size()
     pygame.display.set_caption('Space occupants')
     clock = pygame.time.Clock()
 
-    player = SpaceShip()
-    border = Border(100, 250, 500)
-    for i in range(4):
-        for j in range(10):
-            a = Enemy(choice([0, 1]))
-            a.rect = a.rect.move(50 + 70 * j, 100 + 70 * i)
+    player = SpaceShip(SIZE)
+    border = [Border(SIZE, 100, 250, SIZE[1] - 300, 1),
+              Border(SIZE, SIZE[0] - 250, SIZE[0] - 100, SIZE[1] - 300, -1)]
+    a = Enemy(SIZE, choice((0, 1)))
+    a.rect = a.rect.move(choice(range(50, SIZE[0] - 50)), 0)
 
     running = True
+    tick = True
+    counter = 1
+    k = 1
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pass
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 player.fire()
+        if not counter % int(200 * k):
+            counter = 0
+            k = max(k - 0.01, 0.3)
+            n = choice((1, 2, 3))
+            place = my_choices(range(30, SIZE[0] - 30), n)
+            for i in range(n):
+                if choice((0, 1, 2)):
+                    a = Enemy(SIZE, choice([0, 1]))
+                    a.rect = a.rect.move(place[i], 0)
+                else:
+                    Bullet(SIZE, place[i], 0, choice(range(2, 5)), bulletsb)
         all_sprites.update()
-
         screen.fill((100, 100, 100))
         all_sprites.draw(screen)
         pygame.display.flip()
-        clock.tick(100)
+        if tick:
+            clock.tick(100)
+        counter += 1
     pygame.quit()
 
 
--- ships.py
+++ ships.py
@@ -1,12 +1,13 @@
 import pygame
 import os
+from random import choice
 
 all_sprites = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
-
-SIZE = (800, 700)
+bulletsa = pygame.sprite.Group()
+bulletsb = pygame.sprite.Group()
+borders = pygame.sprite.Group()
 
 
 def load_image(name, color_key=None):
@@ -25,73 +26,88 @@ def load_image(name, color_key=None):
     return image
 
 
-class SpaceShip(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__(all_sprites, player_group)
+def my_choices(seq, k):
+    s = list(seq)
+    res = []
+    for i in range(k):
+        a = choice(s)
+        del s[s.index(a) - 55: s.index(a) + 55]
+        res.append(a)
+    return res
+
+
+class MySprite(pygame.sprite.Sprite):
+    def __init__(self, size, *groups):
+        super().__init__(all_sprites, *groups)
+        self.size = size
+
+
+class SpaceShip(MySprite):
+    def __init__(self, size):
+        super().__init__(size, player_group)
         self.image = pygame.transform.scale(load_image('main_character.png', -1), (84, 65))
-        self.rect = self.image.get_rect().move(self.image.get_rect().x, 600)
+        self.rect = self.image.get_rect().move(self.image.get_rect().x, self.size[1] - 100)
 
     def update(self):
         x = pygame.mouse.get_pos()[0]
         x2 = x - self.rect.x - 0.5 * self.rect.width
-        if 0 <= self.rect.x + x2 and self.rect.x + x2 + self.rect.width <= SIZE[0]:
+        if 0 <= self.rect.x + x2 and self.rect.x + x2 + self.rect.width <= self.size[0]:
             self.rect = self.rect.move(x2, 0)
+        if pygame.sprite.spritecollide(self, bulletsb, True):
+            pass
 
     def fire(self):
-        b = Bullet(self.rect.x + self.rect.width // 2, self.rect.y)
-        return b
+        Bullet(self.size, self.rect.x + self.rect.width // 2, self.rect.y, -3, bulletsa)
 
 
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, im):
-        super().__init__(all_sprites, enemies)
-        self.image = pygame.transform.scale(load_image('enemy1.jpg', -1), (50, 50)) if im == 1 \
-            else pygame.transform.scale(load_image('enemy2.jpg', -1), (50, 50))
+class Enemy(MySprite):
+    def __init__(self, size, i):
+        super().__init__(size, enemies)
+        img = 'enemy1.jpg' if i else 'enemy2.jpg'
+        self.image = pygame.transform.scale(load_image(img, -1), (50, 50))
         self.rect = self.image.get_rect()
         self.counter = 1
         self.k = 5
 
     def moving(self):
-        if not self.counter % 900:
-            self.counter = 1
-            self.rect = self.rect.move(0, 5)
-            self.k = -self.k
-        elif not self.counter % 500:
-            self.k = -self.k
-            self.rect = self.rect.move(self.k, 0)
-        elif not self.counter % 100:
-            self.rect = self.rect.move(self.k, 0)
-        self.counter += 1
+        if not pygame.sprite.spritecollideany(self, borders):
+            self.rect = self.rect.move(0, 1)
 
     def update(self):
-        if pygame.sprite.spritecollide(self, bullets, True):
+        if self.rect.y == self.size[1]:
             self.kill()
+        if pygame.sprite.spritecollide(self, bulletsa, True):
+            self.kill()
+        if choice(range(0, 10000)) < 5:
+            Bullet(self.size, self.rect.x + self.rect.width // 2, self.rect.y, 3, bulletsb)
         self.moving()
 
 
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__(all_sprites, bullets)
+class Bullet(MySprite):
+    def __init__(self, size, x, y, v, b):
+        super().__init__(size, b)
         self.image = load_image('bullet.jpg', -1)
-        self.rect = self.image.get_rect().move(x - 3, y - 25)
+        self.v = v
+        self.rect = self.image.get_rect().move(x - 3, y + self.v * 10)
 
     def update(self):
-        if self.rect.y <= -46:
+        if self.rect.y <= -46 or self.rect.y >= self.size[1]:
             self.kill()
         else:
-            self.rect = self.rect.move(0, -3)
+            self.rect = self.rect.move(0, self.v)
 
 
-class Border(pygame.sprite.Sprite):
-    def __init__(self, x1, x2, y):
-        super().__init__(all_sprites, enemies)
+class Border(MySprite):
+    def __init__(self, size, x1, x2, y, v):
+        super().__init__(size, borders)
         self.image = pygame.Surface([x2 - x1, 10])
         self.image.fill((255, 255, 255))
         self.rect = pygame.Rect(x1, y, x2 - x1, 10)
-        self.v = 1
+        self.v = v
 
     def update(self):
-        pygame.sprite.spritecollide(self, bullets, True)
+        pygame.sprite.spritecollide(self, pygame.sprite.Group(*bulletsa.sprites(),
+                                                              *bulletsb.sprites()), True)
         self.rect = self.rect.move(self.v, 0)
-        if self.rect.x == 0 or self.rect.x + self.rect.width == SIZE[0]:
+        if self.rect.x == 0 or self.rect.x + self.rect.width == self.size[0]:
             self.v = -self.v
